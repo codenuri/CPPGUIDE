0426b6b0316604a9d0586bff872769500a700cec
ecourse <smkang@codenuri.co.kr>
2023-06-07 14:12:27 +0900

    Add files via upload

--- DAY1/2_const_member_function_05-1.cpp
+++ DAY1/2_const_member_function_05-1.cpp
@@ -10,7 +10,8 @@ public:
 
 	~vector() { delete[] buff; }
 
-	T& operator[](int idx) { return buff[idx]; }
+//	T& operator[](int idx)       { return buff[idx]; }
+	T& operator[](int idx) const { return buff[idx]; }
 };
 
 
@@ -20,12 +21,15 @@ int main()
 
 	vector<int> v1(10);
 	const vector<int> v2(10);
+	// 참고
+	// v2 자체만 상수 이고, 할당한 메모리(버퍼)는 상수가 아니지만
+	// 외부에서 사용자가 사용할때는 버퍼도 상수 처럼 보여야 합니다.
 
 	// 아래 4줄을 잘 생각해 보세요
-	//					원하는동작	현재 상태
-	n1 = v1[0];
-	n1 = v2[0];
+	//			 원하는동작	[]가 non-const  [] 가 const
+	n1 = v1[0]; // O		O				O
+	n1 = v2[0]; // O		X				O
 
-	v1[0] = 10;
-	v2[0] = 10;
+	v1[0] = 10; // O		O				O
+	v2[0] = 10; // X		X				O
 }
