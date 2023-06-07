2d136f5fbddc926b22dfdaac96cba8ce9afc30f2
ecourse <smkang@codenuri.co.kr>
2023-06-07 09:45:51 +0900

    Add files via upload

--- DAY1/1_함수인자_02.cpp
+++ DAY1/1_함수인자_02.cpp
@@ -21,3 +21,14 @@ int main()
 	bad(n);
 	good(n);
 }
+
+// 정리
+// 인자를 "읽기만(in parameter)" 한다면
+
+// 1. 인자의 타입이 primitive(int, double 등 언어가 지원하는 기본 타입)
+//    인경우
+// => call by value!
+// ex) good(int n);
+// 
+// 2. user define type : const reference
+// ex) good(const Rect& r);
