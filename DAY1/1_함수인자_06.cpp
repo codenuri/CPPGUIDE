c0f9f83172a1838af550301775363a814d07a5a2
ecourse <smkang@codenuri.co.kr>
2023-06-07 11:44:01 +0900

    Add files via upload

--- DAY1/1_함수인자_06.cpp
+++ DAY1/1_함수인자_06.cpp
@@ -27,3 +27,26 @@ int main()
 //    안전 합니다.
 
 // 2. 포인터는 out parameter 의 의미가 아닌 "위치"의 의미 일때만 사용
+
+// 총정리
+// 1. 인자를 읽기만 하는 경우 - in parameter
+// 
+// => primitive 는 call by value : f1(int n)
+// => UDT 는 call by const reference : f1(const Rect& r)
+
+// => 문자열은 string_view : f1(std::string_view s);
+// => 반복자는 call by value : f1( T iterator)
+
+// 2. 인자를 쓰기도 하는 경우 - out parameter
+// => non-const reference : f1(int& out)
+// => 위치의 의미는 포인터   : f1(int* pos)
+
+// 3. 인자값을 멤버에 보관하는 경우 - move 지원
+// => 인자가 한개라면 2개의 setter 
+//    set_name(const std::string& s) { name = s;}
+//    set_name(std::string&& s) { name = std::move(s);}
+
+// => 인자가 2개 이상이면 T&& 사용 4-1번 소스 참고.
+
+// 4. 인자를 다른 함수에 전달하는 경우 - forwaring 파라미터
+// => T&& 사용 - 5-1번 소스 참고.
