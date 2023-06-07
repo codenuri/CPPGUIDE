8a51c5c8060bacef388434aa616a23ffcb7122aa
ecourse <smkang@codenuri.co.kr>
2023-06-07 13:46:32 +0900

    Add files via upload

--- DAY1/2_const_member_function_04-1.cpp
+++ DAY1/2_const_member_function_04-1.cpp
@@ -3,15 +3,17 @@
 class Test
 {
 public:
-	// 동일이름의 함수는 const, non-const 로 만들수 있습니다.
+	// 동일이름의 함수를 const, non-const 로 만들수 있습니다.
 	void foo()       { std::cout << "foo()\n"; }
 	void foo() const { std::cout << "foo() const\n"; }
 };
 int main()
 {
-	Test t;
-	t.foo();
+	Test t;  // 상수 객체가 아님.
+	t.foo(); // 1. "foo()" 가 먼저 선택됨.. 없다면
+			 // 2. "foo() const" 가 선택됨. 
 
 	const Test ct;
-	ct.foo();
+	ct.foo(); // "foo() const" 만 호출가능. 
+			  // 없다면 error
 }
