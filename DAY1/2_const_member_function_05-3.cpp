5c233243e190f08c17512ae3feb76b1a2585e41d
ecourse <smkang@codenuri.co.kr>
2023-06-07 14:27:49 +0900

    Add files via upload

--- DAY1/2_const_member_function_05-3.cpp
+++ DAY1/2_const_member_function_05-3.cpp
@@ -8,9 +8,9 @@ int main()
 
 	// 동일이름을 가지는 const, non-const 함수를 동시에 제공하는
 	// 경우는 아주 많습니다.
-	auto p1 = v1.begin();
-	auto p2 = v2.begin();
+	auto p1 = v1.begin(); // iterator begin() {}
+	auto p2 = v2.begin(); // const_iterator begin() const {}
 
-	*p1 = 10;
-	*p2 = 10;
+	*p1 = 10; // 쓰기 가능한 반복자 ( iterator )
+	*p2 = 10; // 쓰기 불가능한 반복자 ( const_iterator )
 }
