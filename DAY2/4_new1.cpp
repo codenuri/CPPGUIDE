cc5cc79a41a1688b790674383031b5d9f7dfa4f2
ecourse <smkang@codenuri.co.kr>
2023-06-08 13:33:32 +0900

    Add files via upload

--- DAY2/4_new1.cpp
+++ DAY2/4_new1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 class Point
 {
@@ -25,15 +26,23 @@ int main()
 	Point* p2 = static_cast<Point*>( operator new( sizeof(Point) )); // malloc 과 유사
 
 	
-	// 2. 이미 할당된 메모리에 생성자만 호출
-	new(p2) Point(1, 2);
+	// 2. 이미 할당된 메모리에 생성자만 호출 => placement new 라고 불리는 기술입니다.
+	//new(p2) Point(1, 2);
+	std::construct_at(p2, 1, 2); // C++20 부터 제공
 
 
 	// 3. 메모리 해지 없이 소멸자만 호출
-	p2->~Point();
+//	p2->~Point();
+	std::destroy_at(p2); // C++17 부터 제공되는 함수 <memory> 헤더
 
 
 	// 4. 소멸자 호출 없이 메모리만 제거
 	operator delete(p2); // free
-
 }
+
+// malloc : 메모리 할당
+// new    : 객체의 생성( 메모리 할당 + 생성자 호출 )
+
+
+// new     Point(1,2) : 새로운 메모리에 객체를 생성해 달라는 의미
+// new(p2) Point(1,2) : 이미 할당되어 있는 메모리(p2)에 객체를 생성해 달라는 것
