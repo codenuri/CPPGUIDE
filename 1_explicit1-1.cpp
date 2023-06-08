1076e5530ec6718d0cc08cf3e1d42ccd8d53f2a7
ecourse <smkang@codenuri.co.kr>
2023-06-08 09:22:19 +0900

    Add files via upload

--- 1_explicit1-1.cpp
+++ 1_explicit1-1.cpp
@@ -7,12 +7,15 @@ void f2(std::vector<int> v) {}
 
 int main()
 {
-	f1("hello");
+	f1("hello"); // ok.. 
+				 // 생성자가 explicit 가 아니라는 의미
+	 
+	std::string s1("hello");  // ok
+	std::string s2 = "hello"; // ok
+					// string(const char* ) 생성자는 explicit 아님.
 
-	std::string s1("hello");
-	std::string s2 = "hello";
-
-	f2(10);
-	std::vector<int> v1(10);
-	std::vector<int> v2 = 10;
+	f2(10); // error
+	std::vector<int> v1(10);  // ok
+	std::vector<int> v2 = 10; // error.
+					// vector(int) 의 생성자는 explicit 라는 의미.
 }
