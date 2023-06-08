895d5cf225ed7389a74b293fb865de25744f61ac
ecourse <smkang@codenuri.co.kr>
2023-06-08 13:19:05 +0900

    Add files via upload

--- DAY2/3_virtual2-1.cpp
+++ DAY2/3_virtual2-1.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// C++ Core Guideline 저자
+// C++ 창시자
+// herb shutter => MS 수석아키텍쳐 - VS 팀에서 
+//				Coreguide line 검사 툴을 만들려고 시도 했음.
+
 // C35. 기반 클래스 소멸자는 public - virtual 이거나
 //      protected - non virtual 이어야 한다.
 
@@ -11,6 +16,7 @@ public:
 protected:
 	~Base() { std::cout << "~Base()" << std::endl; }
 };
+
 class Derived : public Base
 {
 public:
