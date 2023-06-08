db1ae03e78f97c447bef5dd40aa53e2d24762e19
ecourse <smkang@codenuri.co.kr>
2023-06-08 16:46:03 +0900

    Add files via upload

--- DAY2/6_make6.cpp
+++ DAY2/6_make6.cpp
@@ -10,7 +10,9 @@ int main()
 	// 거꾸로 바로보는 view 에서 다시 3개만 볼수 있는 view 를 연결
 	auto view = std::views::take( std::views::reverse(v), 3 );
 
-	for (auto e : view )
+	auto view2 = std::views::filter(view, [](int n) { return n % 2 == 0; });
+
+	for (auto e : view2 )
 	{
 		std::cout << e << ", ";
 	}
