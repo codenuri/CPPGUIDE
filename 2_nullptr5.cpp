46fec97f0aef1139e8c27d5e887ed07f2bbc0c9e
ecourse <smkang@codenuri.co.kr>
2023-06-08 11:26:45 +0900

    Add files via upload

---
+++ 2_nullptr5.cpp
@@ -0,0 +1,14 @@
+// 2_nullptr5.cpp
+#include <iostream>
+
+int main()
+{
+	// 1. nullptr 타입 알아 두세요
+	std::nullptr_t mynull = nullptr;
+
+	// 2. 이제 mynull 을 nullptr 대신 사용가능합니다.
+	int* p1  = mynull;
+	char* p2 = mynull;
+
+
+}
