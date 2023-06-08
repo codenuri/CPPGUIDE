0c707eb8dbb3aeba41867e9d482d220c6dc45e59
ecourse <smkang@codenuri.co.kr>
2023-06-08 09:33:18 +0900

    Add files via upload

--- 1_explicit2-1.cpp
+++ 1_explicit2-1.cpp
@@ -1,15 +1,18 @@
 // 1_explicit2-1.cpp
 // C++20 부터는 explicit(bool) 이 추가되었습니다.
+#include <type_traits>
 
 template<typename T>
 class Any
 {
 public:
-	explicit(false) Any(const T& a) {}
+	explicit( std::is_integral_v<T> ) Any(const T& a) {}
 };
 
 int main()
 {
-	Any<int> a1 = 10;
-	Any<double> a2 = 3.4;
+//	Any<int> a1 = 10; // error
+	Any<int> a2( 10); // ok
+
+	Any<double> a3 = 3.4; // ok
 }
