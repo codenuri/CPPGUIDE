b65c3cb1f23bf91b170b22ecc21d6d5baa558ca4
ecourse <smkang@codenuri.co.kr>
2023-06-09 15:38:51 +0900

    Add files via upload

--- DAY3/4_lambda7-1.cpp
+++ DAY3/4_lambda7-1.cpp
@@ -3,8 +3,18 @@ int main()
 {
 	auto swap1 = [](auto& a, auto& b) { auto c = a; a = b; b = c; };
 
-	int n1 = 10;
+	int    n1 = 10;
 	double d1 = 3.4;
 
-	swap1(n1, d1); // 되는게 좋을까요 ? 에러가 좋을까요 ?
+	swap1(n1, d1);	// 되는게 좋을까요 ? 에러가 좋을까요 ?
+					// 에러 아님.
+					// generic lambda(auto인자)는 2개가 서로 다른 템플릿이므로
+
+
+	// C++20 에서 추가된 "template lambda" 라는 문법.
+	auto swap2 = []<typename T>(T& a, T& b) { auto c = a; a = b; b = c; };
+
+	swap2(n1, d1); // error
+	swap2(n1, n1); // ok
+
 }
