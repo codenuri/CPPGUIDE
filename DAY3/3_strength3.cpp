cbbc5095a2f691401855447d624790ed0a82b91a
ecourse <smkang@codenuri.co.kr>
2023-06-09 13:04:12 +0900

    Add files via upload

--- DAY3/3_strength3.cpp
+++ DAY3/3_strength3.cpp
@@ -8,9 +8,11 @@
 
 class Object;	// 클래스 전방 선언
 				// 전방선언만 있으면 포인터 변수를 생성가능합니다.
-
+				// 불완전한(incomplete) 타입 이라고 합니다.
+				// => delete 시에 소멸자가 호출되지 않은 문제가 있습니다.
 void deleter(Object* p)
 {
+	// imcomplete type 은 크기를 구할수 없습니다.
 	static_assert(sizeof(Object) > 0,
 		"error, delete incomplete type");
 
