e7ed8858b933c1d254a50b5cc648a64bbdc53fe9
ecourse <smkang@codenuri.co.kr>
2023-06-09 13:35:25 +0900

    Add files via upload

--- DAY3/3_strength6.cpp
+++ DAY3/3_strength6.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <type_traits>
 
 template<typename T>
 class Complex
@@ -7,8 +8,23 @@ class Complex
 public:
 	Complex(const T& r, const T& i) : real(r), image(i) {}
 
+	/*
 	template<typename U>
-	Complex(const Complex<U>& c) : real(c.real), image(c.image) {}
+	Complex(const Complex<U>& c) : real(c.real), image(c.image) 
+	{
+	}
+	*/
+
+	// enable_if 기술 : 조건을 만족하지 않은 경우에는 템플릿을 사용하지 않게 하는 기술.
+
+	template<typename U,
+			 typename = std::enable_if_t< std::is_convertible_v<U, T> >>
+
+	Complex(const Complex<U>& c) : real(c.real), image(c.image)
+	{
+	}
+
+
 
 	template<typename U> friend class Complex;
 };
@@ -16,5 +32,6 @@ public:
 int main()
 {
 	Complex<std::string> c1("3", "2");
-	Complex<int> c2 = c1;
+
+	Complex<int> c2 = c1;	// 빌드해서.. 에러 메세지 확인해 보세요.
 }
